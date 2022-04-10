c52beed28aa3088be53997d3f0d526a291354add
Max <79434404+MaximusMMM@users.noreply.github.com>
2022-04-10 16:26:40 -0400

    Update League Randomizer.py

--- League Randomizer.py
+++ League Randomizer.py
@@ -5,15 +5,15 @@ styles = ["Tank", "Lethality", "Full AP", "Attack Speed", "Support"]
 
 def generateChamp(champFile):
     with open(champFile) as f:
-        contents_of_file = f.read()
-    lines = contents_of_file.splitlines()
+        contents = f.read()
+    lines = contents.splitlines()
     line_number = random.randrange(0, len(lines))
     return lines[line_number]
 
 def generateSynergies(tftFile):
     with open(tftFile) as f:
-        contents_of_file = f.read()
-    lines = contents_of_file.splitlines()
+        contents = f.read()
+    lines = contents.splitlines()
     line_number = random.randrange(0, len(lines))
     return lines[line_number]
 
